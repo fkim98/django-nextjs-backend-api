13760786e1fe5cc43974d75f2249f8473af69a1a
Frank Kim <fkim98@hotmail.com>
2024-09-25 21:08:41 -0700

    Initial commit again

---
+++ src/waitlists/api.py
@@ -0,0 +1,57 @@
+from typing import List
+import json
+from django.shortcuts import get_object_or_404
+from ninja import Router
+
+import helpers
+from ninja_jwt.authentication import JWTAuth
+
+from .forms import WaitlistEntryCreateForm
+from .models import WaitlistEntry
+from .schemas import (
+  WaitlistEntryCreateSchema,
+  WaitlistEntryListSchema,
+  WaitlistEntryDetailSchema,
+  ErrorWaitlistEntryCreateSchema
+)
+
+router = Router()
+
+
+# /api/waitlist/
+@router.get("", response=List[WaitlistEntryListSchema], auth=helpers.api_auth_user_required)
+def list_waitlist_entries(request):
+  qs = WaitlistEntry.objects.filter(user=request.user)
+  return qs
+
+# /api/waitlist/
+@router.post(
+  "",
+  response={
+    201: WaitlistEntryDetailSchema,
+    400: ErrorWaitlistEntryCreateSchema,
+  },
+  auth=helpers.api_auth_user_or_annon
+)
+def create_waitlist_entry(request, data:WaitlistEntryCreateSchema):
+  form = WaitlistEntryCreateForm(data.dict())
+  if not form.is_valid():
+    # cleaned_data = form.cleaned_data
+    # obj = WaitlistEntry(**data.dict())
+    # {'email': [{'message': 'Cannot use gmail', 'code': ''}]}
+    form_errors = json.loads(form.errors.as_json())
+    return 400, form_errors
+  obj = form.save(commit=False)
+  if request.user.is_authenticated:
+    obj.user = request.user
+  obj.save()
+  return 201, obj
+
+@router.get("{entry_id}/", response=WaitlistEntryDetailSchema, auth=helpers.api_auth_user_required)
+def get_waitlist_entry(request, entry_id:int):
+  obj = get_object_or_404(
+    WaitlistEntry,
+    id=entry_id,
+    user=request.user
+  )
+  return obj
---
+++ src/waitlists/schemas.py
@@ -0,0 +1,29 @@
+from typing import List, Any
+from datetime import datetime
+from ninja import Schema
+from pydantic import EmailStr
+
+class WaitlistEntryCreateSchema(Schema):
+  # Create -> Data
+  # WaitlistEntryIn
+  email: EmailStr
+
+class ErrorWaitlistEntryCreateSchema(Schema):
+  # Create -> Data
+  # WaitlistEntryIn
+  email: List[Any]
+  # non_field_errors: List[dict] = []
+
+class WaitlistEntryListSchema(Schema):
+  # List -> Data
+  # WaitlistEntryOut
+  id: int
+  email: EmailStr
+
+class WaitlistEntryDetailSchema(Schema):
+  # Get -> Data
+  # WaitlistEntryOut
+  id: int
+  email: EmailStr
+  updated: datetime
+  timestamp: datetime
